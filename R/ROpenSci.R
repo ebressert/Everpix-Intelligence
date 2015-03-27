4db2e196ef1bc0f787621e2a988dbdf101cf677e
Gabriela de Queiroz <gabidequeiroz@gmail.com>
2015-03-27 14:13:43 -0700

    add funtions to explore the data

--- R/ROpenSci.R
+++ R/ROpenSci.R
@@ -1,8 +1,60 @@
-
-
+# Load packages
+library(dplyr)
+library(lubridate)
+library(tidyr)
 ## Dir: external_metrics
 
 aws_billing <- read.csv("external_metrics/aws_billing_from_2012_10.csv", stringsAsFactors = FALSE)
+str(aws_billing) # 1584 obs. of  12 variables
 
-read.csv("working/all.csv", )
+aws_billing$UsageStartDate <- ymd_hms(aws_billing$UsageStartDate)
+aws_billing$UsageEndDate <- ymd_hms(aws_billing$UsageEndDate)
 
+
+head(aws_billing)
+summary(aws_billing)
+
+aws_billing %>%
+  ggplot(aes(x = TotalCost)) +
+  geom_histogram()
+
+
+
+
+
+# Total Cost per Product
+aws_billing %>% group_by(ProductCode) %>%
+  summarise(sum_price = sum(TotalCost)) %>%
+  summary()
+
+aws_billing %>% group_by(ProductCode) %>%
+  summarise(sum_TotalCost = sum(TotalCost)) %>%
+  ggplot(aes(x =ProductCode, y = sum_TotalCost)) +
+  geom_bar(stat = "identity")
+
+aws_billing %>% group_by(ProductCode) %>%
+  summarise(sum_TotalCost = sum(TotalCost)) %>%
+  ggplot(aes(x =ProductCode, y = sum_TotalCost)) +
+  geom_boxplot()
+
+
+
+# Total Cost per Product and Usage Type
+aws_billing %>% group_by(ProductCode, UsageType) %>%
+  summarise(sum_TotalCost = sum(TotalCost)) %>%
+  summary()
+
+aws_billing %>% group_by(ProductCode, UsageType) %>%
+  summarise(sum_TotalCost = sum(TotalCost)) %>%
+  filter(sum_TotalCost)
+
+ggplot(aes(x = UsageType, y = sum_price)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ProductCode)
+
+
+
+  
+  
+
+  
